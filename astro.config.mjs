4c3c21c04810670c834f11b3d21d13ea9d4164a2
Xavimc04 <xaviermorcam@campus.monlau.com>
2023-12-19 23:39:16 +0100

    update(menu): Interface and cart design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
+import astroI18next from "astro-i18next"; 
 
 import react from "@astrojs/react";
 
--- src/components/dynamic/products.jsx
+++ src/components/dynamic/products.jsx
@@ -6,7 +6,7 @@ export default function Products({
     extend = false, 
 }) {
     const [data, handleData] = useState([])
-    const [current, setCurrent] = useState(0)
+    const [current, setCurrent] = useState(0) 
 
     useEffect(() => {
         const pathWithoutSlashes = window.location.pathname.replace(/^\/|\/$/g, '');
@@ -19,7 +19,7 @@ export default function Products({
             .then(data => handleData(data))
     }, [])
 
-    return <div className="flex flex-col py-10">
+    return <div className="flex flex-col py-10 h-screen w-[1100px] bg-white px-10 rounded">
         <section className="flex flex-wrap gap-5">
             {
                 data.categories && data.categories.map((category, index) => {
@@ -35,20 +35,22 @@ export default function Products({
         </section>
 
         <section className="flex flex-col md:flex-row gap-7 md:gap-0 mt-10 h-auto md:h-[500px]">
-            <div className="flex-1 bg-gray-50 rounded flex items-center justify-center">
-                <img 
-                    src={ data.categories && data.categories[current] && data.categories[current].image }
-                    className="w-[450px]"
-                    draggable="false"
-                />
-            </div>
+            {
+                !extend && <div className="flex-1 bg-gray-50 rounded flex items-center justify-center">
+                    <img 
+                        src={ data.categories && data.categories[current] && data.categories[current].image }
+                        className="w-[450px]"
+                        draggable="false"
+                    />
+                </div>
+            }
 
             <div className="flex-1 flex flex-col px-5">
                 {
-                    data.products && data.products.filter(product => product.category == data.categories[current].name).slice(0, 5).map((product, index) => {
+                    data.products && data.products.filter(product => product.category == data.categories[current].name).slice(0, extend ? -1 : 5).map((product, index) => {
                         return <div className="flex py-3 items-center border-b border-dotted" key={ index }>
-                            <div className="flex-1 flex items-center justify-between">
-                                <section className="flex flex-col">
+                            <div className="flex-1 flex items-center gap-5">
+                                <section className="flex flex-col flex-1">
                                     <h3 className="text-lg font-bold">
                                         { product.name }
                                     </h3>
@@ -61,6 +63,14 @@ export default function Products({
                                 <p className="text-md  text-red-500">
                                     €{ product.price }
                                 </p> 
+
+                                <button className="border border-black text-black hover:border-red-400 hover:text-red-400 transition-all rounded-sm px-5 py-2 flex items-center gap-3">
+                                    <span className='material-symbols-outlined'>
+                                        shopping_cart
+                                    </span>
+
+                                    Añadir
+                                </button>
                             </div>
                         </div>
                     })
