2d0f140a98015786ca8b557c4c8d15c044fbb260
Xavimc04 <xaviermorcam@campus.monlau.com>
2024-02-19 16:52:28 +0100

    Responsive on products interface

--- src/components/dynamic/products.jsx
+++ src/components/dynamic/products.jsx
@@ -44,7 +44,7 @@ export default function Products({
             .then(data => handleData(data))
     }, [])
 
-    return <div className="flex flex-col py-10 h-screen w-[1100px] bg-white px-10 rounded mt-10">
+    return <div className="flex flex-col py-10 lg:h-screen w-[90%] lg:w-[1100px] bg-white px-10 rounded mt-10">
         <section className="flex flex-wrap gap-5">
             {
                 data.categories && data.categories.map((category, index) => {
@@ -59,70 +59,74 @@ export default function Products({
             }
         </section>
 
-        <section className="flex flex-col gap-7 md:gap-0 mt-10 h-auto md:h-[500px]">
+        <section className="flex flex-col gap-7 md:gap-0 mt-10">
             {
                 data.products && data.products.filter(product => product.category == data.categories[current].name).slice(0, extend ? -1 : 5).map((product, index) => {
                     const storedProduct = products.find(res => res.name == product.name)
 
                     return <div className="flex py-3 items-center border-b border-dotted" key={ index }>
-                        <div className="flex-1 flex items-center gap-5">
-                            <img src={ product.image } className="w-16 h-16 p-3 rounded-md select-none cursor-pointer" />
+                        <div className="flex-1 flex flex-wrap items-center justify-end gap-5">
+                            <article className='flex items-center flex-wrap gap-5 w-full lg:w-auto lg:flex-1'>
+                                <img src={ product.image } className="w-full md:w-16 md:h-16 p-3 rounded-md select-none cursor-pointer" />
 
-                            <section className="flex flex-col flex-1">
-                                <h3 className="text-lg font-bold">
-                                    { product.name }
-                                </h3>
+                                <section className="flex flex-col flex-1">
+                                    <h3 className="text-lg font-bold">
+                                        { product.name }
+                                    </h3>
 
-                                <p className="text-sm mt-1 text-gray-500">
-                                    { product.description }
+                                    <p className="text-sm mt-1 text-gray-500">
+                                        { product.description }
+                                    </p> 
+                                </section>
+
+                                <p className="text-xl text-red-500">
+                                    { product.price }
+
+                                    <span className='text-sm pl-1 text-black/30'>
+                                        €
+                                    </span>
                                 </p> 
-                            </section>
+                            </article>
 
-                            <p className="text-xl text-red-500">
-                                { product.price }
-
-                                <span className='text-sm pl-1 text-black/30'>
-                                    €
-                                </span>
-                            </p> 
-
-                            {
-                                storedProduct && storedProduct.quant && <div className="flex items-center gap-3">
-                                    <button onClick={() => {
-                                        if(storedProduct.quant == 1) return setProducts(products.filter(res => res.name != product.name))
-
-                                        handleQuant(product, storedProduct.quant - 1)
-                                    }} className="border border-black w-8 h-8 rounded-full flex items-center justify-center">
-                                        <span className='material-symbols-outlined'>
-                                            remove
-                                        </span>
-                                    </button>
-
-                                    <p className="text-lg w-8 flex justify-center">
-                                        { storedProduct.quant }
-                                    </p>
-
-                                    <button onClick={() => handleQuant(product, storedProduct.quant + 1)} className="border border-black w-8 h-8 rounded-full flex items-center justify-center">
-                                        <span className='material-symbols-outlined'>
-                                            add
-                                        </span>
-                                    </button>
-                                </div>
-                            }
-
-                            <button onClick={ () => handleProduct(product) } className="border border-black w-36 justify-center text-black hover:border-red-400 hover:text-red-400 transition-all rounded-sm px-5 py-2 flex items-center gap-3">
+                            <section className='flex items-center justify-center w-full md:w-auto gap-5'>
                                 {
-                                    storedProduct ? <>
-                                        <span className='material-symbols-outlined'>
-                                            close
-                                        </span>
-                                    </> : <>
-                                        <span className='material-symbols-outlined'>
-                                            shopping_cart
-                                        </span>
-                                    </>
+                                    storedProduct && storedProduct.quant && <div className="flex items-center gap-3">
+                                        <button onClick={() => {
+                                            if(storedProduct.quant == 1) return setProducts(products.filter(res => res.name != product.name))
+
+                                            handleQuant(product, storedProduct.quant - 1)
+                                        }} className="border border-black w-8 h-8 rounded-full flex items-center justify-center">
+                                            <span className='material-symbols-outlined'>
+                                                remove
+                                            </span>
+                                        </button>
+
+                                        <p className="text-lg w-8 flex justify-center">
+                                            { storedProduct.quant }
+                                        </p>
+
+                                        <button onClick={() => handleQuant(product, storedProduct.quant + 1)} className="border border-black w-8 h-8 rounded-full flex items-center justify-center">
+                                            <span className='material-symbols-outlined'>
+                                                add
+                                            </span>
+                                        </button>
+                                    </div>
                                 }
-                            </button>
+
+                                <button onClick={ () => handleProduct(product) } className="border border-black w-full md:w-36 justify-center text-black hover:border-red-400 hover:text-red-400 transition-all rounded-sm px-5 py-2 flex items-center gap-3">
+                                    {
+                                        storedProduct ? <>
+                                            <span className='material-symbols-outlined'>
+                                                close
+                                            </span>
+                                        </> : <>
+                                            <span className='material-symbols-outlined'>
+                                                shopping_cart
+                                            </span>
+                                        </>
+                                    }
+                                </button>
+                            </section>
                         </div>
                     </div>
                 })
